d1f0479a9fecab9d9b215c91c2dba86de28ca7e2
Edward Hurtado <edhur9@gmail.com>
2022-03-29 22:58:40 -0400

    imported routes/ Routes to different pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import TopBar from "./components/topbar/TopBar";
+import Home from "./pages/homepage/Homepage";
+import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
 function App() {
+  const user = false;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <TopBar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route
+          path="/register"
+          element={user ? <Home /> : <Register />}
+        ></Route>
+        <Route path="/login" element={user ? <Home /> : <Login />}></Route>
+        <Route path="/write" element={user ? <Write /> : <Register />}></Route>
+        <Route
+          path="/settings"
+          element={user ? <Settings /> : <Register />}
+        ></Route>
+        <Route path="/post/:postId" element={<Single />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
